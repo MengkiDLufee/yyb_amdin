f5f53cf14fe452316a8b1eb670f936d6be6b3828
louissa <1721325597@qq.com>
2021-03-01 12:46:38 +0800

    用户管理

--- src/pages/system/user.jsx
+++ src/pages/system/user.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import {
+  Card,
+  Button,
+  Table
+}from 'antd'
 
 export default class User extends Component {
   render() {
+    const title =(
+      <span>
+        <Button type='primary'>搜索</Button>
+        <Button type='primary'>添加</Button>
+        <Button type='primary'>导出</Button>
+      </span>
+    )
     return (
-      <div>
-        用户管理
-      </div>
+      <Card title={title}>
+        <Table
+        bordered
+        rowKey='_id'
+        columns={this.columns}
+        pagination={{defaultPageSize:5,showQuickJumper:true}}
+        />
+      </Card>
     )
   }
 }
